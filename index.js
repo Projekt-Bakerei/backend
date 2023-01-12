0983f4066ae4a57714029e881e160739b5ed314b
Vens Kolev <v.kolev@alfatrex.com>
2023-01-12 09:38:59 +0100

    customers add und get fixet (#1)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import customerRoutes from "./routers/customers.js";
 dotenv.config();
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
@@ -21,7 +22,9 @@ app.get("/", (req, res) => {
   return res.json({ msg: "Mohasebe API up and running" });
 });
 
-mongoose.set("strictQuery", false);
+
+mongoose.set('strictQuery', false);
+
 mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
   app.listen(PORT, () => {
     console.log(`Mohasebe API is running http://localhost:${PORT}`);
