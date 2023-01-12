22340d133ac4157e126cced245a24b30af7a7fda
Vens Kolev <v.kolev@alfatrex.com>
2023-01-12 10:18:08 +0100

    Fix

--- models/customer.js
+++ models/customer.js
@@ -1,4 +1,51 @@
 import mongoose from "mongoose";
+// Empfänger Schema
+const FirmaRechnungSchema = new mongoose.Schema(
+{
+    firmenname: String,
+    firmenadresse: String,
+    firmenstadt: String,
+    ansprechpartner: String,
+  }
+)
+export const FirmaRechnung = mongoose.model("FirmaRechnung", FirmaRechnungSchema)
+
+const ArtickelRechnungSchema = new mongoose.Schema(
+  {
+    beschreibung: String,
+    menge: Number,
+    zwieschensumme: Number,
+    steuer: Number,
+    rabat: Number,
+  }
+  )
+  export const ArtickelRechnung = mongoose.model("ArtickelRechnung", ArtickelRechnungSchema)
+
+const RechnungSchema = new mongoose.Schema(
+  {
+  rechnungsnummern: Number,
+  kundennummer: Number,
+},
+{
+  timestamps: true,
+  versionKey: false,
+  id: true,
+  toJSON: {
+    transform(doc, ret){
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+},
+{
+  firmato: [FirmaRechnungSchema],
+},
+{
+  artickel: [ArtickelRechnung],
+}
+);
+export const Rechnung = mongoose.model("Rechnung", RechnungSchema)
+
 
 const customerSchema = new mongoose.Schema({
  
@@ -28,6 +75,10 @@ const customerSchema = new mongoose.Schema({
       delete ret._id
     }
   }
-})
+},
+{
+  rechnung: [RechnungSchema],
+}
+)
 
 export const Customer = mongoose.model("Customer", customerSchema, "customers");
