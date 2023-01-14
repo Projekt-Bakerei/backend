4ed46163129753632250d50dc6390ecee7e01ea8
Vens Kolev <v.kolev@alfatrex.com>
2023-01-14 13:51:43 +0100

    New Artikel Kodu required

--- models/Artikel.js
+++ models/Artikel.js
@@ -1,24 +1,29 @@
 import mongoose from "mongoose";
 
-const artikelSchema = new mongoose.Schema({
-  NewartikelName: { type: String },
-  NewartikelPrice: { type: String },
-  NewartikelBeschreibung: {  type: String },
-  NewartikelRabat: {  type: String },
-  NewartikelKodu: {  type: String },
+const artikelSchema = new mongoose.Schema(
+  {
+    NewartikelName: { type: String },
+    NewartikelPrice: { type: String },
+    NewartikelBeschreibung: { type: String },
+    NewartikelRabat: { type: String },
+    NewartikelKodu: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
   {
     timestamps: true,
     versionKey: false,
     id: true,
     toJSON: {
-      transform(doc, ret){
-        ret.id = ret._id
-        delete ret._id
-      }
-    }
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-  );
+);
 
 export const AddArtikel = mongoose.model("Artikel", artikelSchema, "artikels");
 //Comment
