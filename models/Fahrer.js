8b3ca26a9105ca6229e48f7f7583a7cda07e8fba
Vens Kolev <v.kolev@alfatrex.com>
2023-02-12 21:44:42 +0100

    Fahre add, list, delete. Miterbeiter add, list, delete erstellt

---
+++ models/Fahrer.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const fahrerSchema = new mongoose.Schema({
+  mName: { type: String },
+  mAdres: { type: String },
+  tel: {  type: String },
+  position: {  type: String },
+  extern: {  type: String },
+  kenzeichen: {  type: String },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    id: true,
+    toJSON: {
+      transform(doc, ret){
+        ret.id = ret._id
+        delete ret._id
+      }
+    }
+  }
+  );
+
+export const Fahrer = mongoose.model("Fahrer", fahrerSchema, "fahrern");
